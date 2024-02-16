dbf358f91657b04519a2f7d4e97f8d92789dceb6
Inupa Vihan <inupavihan9@gmail.com>
2024-02-16 15:25:09 +0530

    update animal

--- animal.py
+++ animal.py
@@ -12,11 +12,11 @@ def default():
 def main():
     if sys.argv[1] == 'dog':
         dog()
-    
-    if sys.argv[1] == 'cat':
-        cat()
     else:
-        default()
+        if sys.argv[1] == 'cat':
+            cat()
+        else:
+            default()
 
 if __name__ == '__main__':
     main()
